3bbb2e82cace0be09d42eb0b50cf99635455d6ea
najibalimudinn <najibaf147@gmail.com>
2023-04-04 10:43:03 +0700

    add grade enum

---
+++ rekapnilai/src/main/java/com/k4/rekapnilai/model/MhsGrade.java
@@ -0,0 +1,35 @@
+package com.k4.rekapnilai.model;
+
+public enum MhsGrade {
+	A(90),
+	AB(85),
+	B(80),
+	BC(75),
+	C(70),
+	D(60),
+	E(0);
+	
+	private int minScore;
+	
+	MhsGrade(int minScore) {
+		this.minScore = minScore;
+	}
+	
+	public static MhsGrade getGrade(int score) {
+		if (score >= A.minScore) {
+			return A;
+		} else if (score >= AB.minScore) {
+			return AB;
+		} else if (score >= B.minScore) {
+			return B;
+		} else if (score >= BC.minScore) {
+			return BC;
+		} else if (score >= C.minScore) {
+			return C;
+		} else if (score >= D.minScore) {
+			return D;
+		} else {
+			return E;
+		}
+	}
+}
