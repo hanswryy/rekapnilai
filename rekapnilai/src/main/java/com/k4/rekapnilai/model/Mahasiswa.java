28b65b6d6ac8334865eba89153a8843ba9f72f52
hanswryy <farhan.hans257@gmail.com>
2023-04-04 11:37:48 +0700

    Mahasiswa change

--- rekapnilai/src/main/java/com/k4/rekapnilai/model/Mahasiswa.java
+++ rekapnilai/src/main/java/com/k4/rekapnilai/model/Mahasiswa.java
@@ -1,13 +1,21 @@
 package com.k4.rekapnilai.model;
 
 public class Mahasiswa{
-	private byte jml_nilai;
+	private int nilaiJml, nilaiUts, nilaiUas, nilaiTugas;
+	private double nilaiAvr;
+	private MhsGrade grade;
 	private String namaMhs, NIM, kelas;
-	public Mahasiswa(String namaMhs, String NIM, String kelas) {
+
+	public void addMahasiswa(String namaMhs, String NIM, String kelas) {
 		this.namaMhs = namaMhs;
 		this.NIM = NIM;
 		this.kelas = kelas;
-		this.jml_nilai = 0;
+		this.nilaiJml = 0;
+		this.nilaiAvr = 0;
+		this.grade = MhsGrade.E;
+		this.nilaiUts = 0;
+		this.nilaiUas = 0;
+		this.nilaiTugas = 0;
 	}
 	
 	public int menghitungNilai() {
