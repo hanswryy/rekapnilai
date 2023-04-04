a44ebe7a00bfebeee9b57ca230d1c81ca5c0612f
yuwan121 <117898042+yuwan121@users.noreply.github.com>
2023-04-04 10:30:31 +0700

    updateclass

---
+++ rekapnilai/src/main/java/com/k4/rekapnilai/Kelas.java
@@ -0,0 +1,12 @@
+package com.k4.rekapnilai;
+
+public class Kelas{
+	private String NIM,kelas;
+	public Kelas(String namaMhs, String NIM, String kelas) {
+		
+		this.NIM = NIM;
+		this.kelas = kelas;
+	}
+	
+	
+}
---
+++ rekapnilai/src/main/java/com/k4/rekapnilai/Mahasiswa.java
@@ -0,0 +1,18 @@
+package com.k4.rekapnilai;
+
+public class Mahasiswa{
+	private byte jml_nilai;
+	private String namaMhs, NIM, kelas;
+	public Mahasiswa(String namaMhs, String NIM, String kelas) {
+		this.namaMhs = namaMhs;
+		this.NIM = NIM;
+		this.kelas = kelas;
+		this.jml_nilai = 0;
+	}
+	
+	public int menghitungNilai() {
+		int hasilNilai = 0;
+		//Menthod menghitung nilai
+		return hasilNilai;
+	}
+}
