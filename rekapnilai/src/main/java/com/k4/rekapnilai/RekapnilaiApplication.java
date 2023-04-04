756d8abcba6c685bc3162daf54dcc4f79902ece5
hanswryy <farhan.hans257@gmail.com>
2023-04-04 09:27:24 +0700

    init commit

---
+++ rekapnilai/src/main/java/com/k4/rekapnilai/RekapnilaiApplication.java
@@ -0,0 +1,13 @@
+package com.k4.rekapnilai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekapnilaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekapnilaiApplication.class, args);
+	}
+
+}
---
+++ rekapnilai/src/test/java/com/k4/rekapnilai/RekapnilaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k4.rekapnilai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekapnilaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
