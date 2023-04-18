7ab6d630f63c1c43e2acca41baf1ec8de0ef7921
Farhan <farhan.muhammad.tif422@polban.ac.id>
2023-04-18 13:41:03 +0700

    test

--- rekapnilai/src/main/java/com/k4/rekapnilai/controller/MahasiswaController.java
+++ rekapnilai/src/main/java/com/k4/rekapnilai/controller/MahasiswaController.java
@@ -4,11 +4,12 @@ import com.k4.rekapnilai.model.Mahasiswa;
 import com.k4.rekapnilai.service.MahasiswaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/mahasiswa")
 public class MahasiswaController {
 
